073cbed1192752590e9dcda0fabf4f28122d3ba5
Siwei LU <me@siwei.lu>
2022-10-10 01:32:39 +0800

    feat: add git diff

---
+++ lua/user/diffview.lua
@@ -0,0 +1,19 @@
+local status_ok, diffview = pcall(require, "diffview")
+if not status_ok then
+  return
+end
+
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+keymap("n", "<C-g>", "<cmd>DiffviewOpen<CR>", opts)
+
+diffview.setup {
+  file_panel = {
+    listing_style = "list", -- One of 'list' or 'tree'
+    win_config = { -- See ':h diffview-config-win_config'
+      width = 30,
+    },
+  },
+}
