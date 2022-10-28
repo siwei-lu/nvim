7691e74c4253cb8915d3b8733cb5d2698eeaca0f
Sway <swaylu@apifox.com>
2022-10-28 15:19:35 +0800

    feat: hide .git folder

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -10,12 +10,16 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
-nvim_tree.setup {
+nvim_tree.setup({
   update_focused_file = {
     enable = true,
     update_cwd = true,
   },
+  filters = {
+    custom = { "^.git$" },
+  },
   renderer = {
+    group_empty = true,
     root_folder_modifier = ":t",
     icons = {
       glyphs = {
@@ -58,10 +62,10 @@ nvim_tree.setup {
     side = "left",
     mappings = {
       list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
+        { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
+        { key = "h", cb = tree_cb("close_node") },
+        { key = "v", cb = tree_cb("vsplit") },
       },
     },
   },
-}
+})
